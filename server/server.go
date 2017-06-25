92b39b19b50831329c13d27fd935b9a91f190d70
Dennis Brandenburg <dbranden@Denniss-MacBook-Pro.local>
2017-06-25 17:13:36 +0200

    Example server and client added

---
+++ server/server.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+  "log"
+  "net"
+
+  pb "event/event.proto"
+  "golang.org/x/net/context"
+  "google.golang.org/grpc"
+)
+
+const (
+  port = ":50051"
+)
+
+type server struct{}
+
+func (s *server) Process(ctx context.Context, in *pb.Event) (*pb.Delivery, error) {
+  return &pb.Event{Message: "Hello "}, nil
+}
+
+func main() {
+  lis, err := net.Listen("tcp", port)
+  if err != nil {
+    log.Fatalf("failed to listen: %v", err)
+  }
+  s := grpc.NewServer()
+  pb.RegisterEventProtosServer(s, &server{})
+  s.Serve(lis)
+}
